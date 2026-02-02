f67040c4d08eefcd99d7219c6c7a13b56a9f1f8e
KandDaHeee <dhk3161@gmail.com>
2026-02-02 16:35:07 +0900

    base:init

---
+++ src/main/java/com/org/dermavera/DermaveraApplication.java
@@ -0,0 +1,13 @@
+package com.org.dermavera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DermaveraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DermaveraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/org/dermavera/DermaveraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.dermavera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DermaveraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
